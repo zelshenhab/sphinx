75f49346bb33439df85ee4352649e162da95a131
Zeyad Elshenhab <z.zelshenhab@devsparkclub.com>
2026-09-02 20:18:58 +0300

    Stop tracking installed dependencies

--- node_modules/@typescript-eslint/eslint-plugin/dist/util/isStartOfArrowFunctionBody.d.ts
+++
@@ -1,2 +0,0 @@
-import type { TSESLint, TSESTree } from '@typescript-eslint/utils';
-export declare function isStartOfArrowFunctionBody(node: TSESTree.Node, sourceCode: TSESLint.SourceCode): boolean;
